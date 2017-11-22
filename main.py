997af7d0681de0d139475b6b9371be08922164b7
Aleksey Lymar <yoptar@gmail.com>
2017-11-22 13:36:30 +0300

    feat: use propabilities instead of weights

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from collections import defaultdict
 
 import csv
 
+from math import log, exp
+
 
 words = DataProvider(short=False).data
 
@@ -15,44 +17,49 @@ words_trie = pygtrie.CharTrie.fromkeys(words, True)
 print('Time spent creating the words trie', time() - s)
 
 alphabet = {c for w in words for c in w}
-costs = defaultdict(lambda: 1)
-costs[('', '')] = 0
+costs = defaultdict(lambda: log(1e-8))
+costs[('', '')] = log(1)
 for c in alphabet:
-    costs[(c, c)] = 0
+    costs[(c, c)] = log(1)
 
 with open('target/probs_ru.tsv') as tsvfile:
     reader = csv.reader(tsvfile, delimiter='\t')
     for l, r, p in reader:
-        costs[(l, r)] = 1-float(p)
+        costs[(l, r)] = log(float(p))
 
 candidates = {}
 
 
-def calc_lev(d, prefixes, w):
+def calc_lev(d, prefixes, w, k=10):
     w = '•' + w.lower().replace('ё', 'е')
-    while prefixes:
+    while k and prefixes:
         new_prefixes = set()
         for prefix in prefixes:
             res = []
             for i, c in enumerate(w):
                 c = c.replace('•', '')
-                res.append(min(
-                    (res[-1] + costs[(prefix[-1] if prefix else '', c)]) if i else float('inf'),
-                    d[prefix[:-1]][i] + costs[(prefix[-1], '')] if prefix else float('inf'),
+                res.append(max(
+                    (res[-1] + costs[(prefix[-1] if prefix else '', c)]) if i else float('-inf'),
+                    d[prefix[:-1]][i] + costs[(prefix[-1], '')] if prefix else float('-inf'),
                     (d[prefix[:-1]][i - 1] + (costs[(prefix[-1], c)] if prefix[-1] != c else 0))
-                    if prefix and i else float('inf')
+                    if prefix and i else float('-inf')
                 ) if i or prefix else 0)
             d[prefix] = res
             if prefix in words_trie:
-                candidates[prefix] = res[-1]
-            if min(res) < 2:
+                candidates[prefix] = exp(res[-1])
+                k -= 1
+            if max(res) > -6.91:  # log(0.001)
                 new_prefixes.update({x[:len(prefix) + 1] for x in words_trie.keys(prefix) if len(x) > len(prefix)})
         prefixes = new_prefixes
 
 
-to_fix = 'превет'
+to_fix = 'коловрат'
 s = time()
 distances = {}
 calc_lev(distances, {''}, to_fix)
 print('Time to do everything else', time() - s)
-print(sorted(candidates.items(), key=lambda x: x[1])[:5])
+
+candidates_short = sorted(candidates.items(), key=lambda x: x[1], reverse=True)[:5]
+
+print(to_fix)
+print(candidates_short)
